479e02d178adf9059a84b5809b76fd89f3ece14e
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-04-09 18:12:16 -0300

    Criação do projeto

---
+++ Serdin/src/main/java/com/serdin/SerdinApplication.java
@@ -0,0 +1,13 @@
+package com.serdin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerdinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerdinApplication.class, args);
+	}
+
+}
